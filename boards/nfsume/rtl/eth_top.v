c3ef339be4bba845e0f2124392789d453ec9386d
Yuta Tokusashi <tokusasi@anago.arc.ics.keio.ac.jp>
2016-08-04 12:20:29 +0900

    Implement done second eth port

--- boards/nfsume/rtl/eth_top.v
+++ boards/nfsume/rtl/eth_top.v
@@ -239,12 +239,12 @@ axi_10g_ethernet_nonshared u_axi_10g_ethernet_1 (
 	.signal_detect           (!ETH1_RX_LOS),         // input wire signal_detect
 	.tx_fault                (ETH1_TX_FAULT),             // input wire tx_fault
 	.tx_disable              (ETH1_TX_DISABLE),         // output wire tx_disable
-	.pcspma_status           (pcspma_status),        // output wire [7 : 0] pcspma_status
+	.pcspma_status           (),        // output wire [7 : 0] pcspma_status
 	.sim_speedup_control     (1'b0),           // input wire sim_speedup_control
 	.rxrecclk_out            (),                  // output wire rxrecclk_out
-	.s_axi_aclk              (s_axi_aclk),             // input wire s_axi_aclk
-	.s_axi_aresetn           (s_axi_aresetn),          // input wire s_axi_aresetn
-	.xgmacint                (xgmacint),               // output wire xgmacint
+	.s_axi_aclk              (clk100),            // input wire s_axi_aclk
+	.s_axi_aresetn           (!eth_rst),          // input wire s_axi_aresetn
+	.xgmacint                (),               // output wire xgmacint
 	.areset_coreclk          (areset_coreclk),         // input wire areset_coreclk
 	.txusrclk                (txusrclk),               // input wire txusrclk
 	.txusrclk2               (txusrclk2),              // input wire txusrclk2
@@ -261,22 +261,22 @@ axi_10g_ethernet_nonshared u_axi_10g_ethernet_1 (
 	.qplloutrefclk           (qplloutrefclk),          // input wire qplloutrefclk
 	.reset_counter_done      (reset_counter_done),     // input wire reset_counter_done
 	// AXI Lite
-	.s_axi_araddr            (s_axi_araddr),           // input wire [10 : 0] s_axi_araddr
+	.s_axi_araddr            (11'b0),                  // input wire [10 : 0] s_axi_araddr
 	.s_axi_arready           (s_axi_arready),          // output wire s_axi_arready
-	.s_axi_arvalid           (s_axi_arvalid),          // input wire s_axi_arvalid
-	.s_axi_awaddr            (s_axi_awaddr),           // input wire [10 : 0] s_axi_awaddr
+	.s_axi_arvalid           (1'b0),                   // input wire s_axi_arvalid
+	.s_axi_awaddr            (11'b0),                  // input wire [10 : 0] s_axi_awaddr
 	.s_axi_awready           (s_axi_awready),          // output wire s_axi_awready
-	.s_axi_awvalid           (s_axi_awvalid),          // input wire s_axi_awvalid
-	.s_axi_bready            (s_axi_bready),           // input wire s_axi_bready
+	.s_axi_awvalid           (1'b0),                   // input wire s_axi_awvalid
+	.s_axi_bready            (1'b0),                   // input wire s_axi_bready
 	.s_axi_bresp             (s_axi_bresp),            // output wire [1 : 0] s_axi_bresp
 	.s_axi_bvalid            (s_axi_bvalid),           // output wire s_axi_bvalid
 	.s_axi_rdata             (s_axi_rdata),            // output wire [31 : 0] s_axi_rdata
-	.s_axi_rready            (s_axi_rready),           // input wire s_axi_rready
+	.s_axi_rready            (1'b0),                   // input wire s_axi_rready
 	.s_axi_rresp             (s_axi_rresp),            // output wire [1 : 0] s_axi_rresp
 	.s_axi_rvalid            (s_axi_rvalid),           // output wire s_axi_rvalid
-	.s_axi_wdata             (s_axi_wdata),            // input wire [31 : 0] s_axi_wdata
+	.s_axi_wdata             (32'd0),                  // input wire [31 : 0] s_axi_wdata
 	.s_axi_wready            (s_axi_wready),           // output wire s_axi_wready
-	.s_axi_wvalid            (s_axi_wvalid),           // input wire s_axi_wvalid
+	.s_axi_wvalid            (1'b0),                   // input wire s_axi_wvalid
 	// AXI Stream Interface
 	.s_axis_tx_tdata         (m_axis_rx1_tdata),        // input wire [63 : 0] s_axis_tx_tdata
 	.s_axis_tx_tkeep         (m_axis_rx1_tkeep),        // input wire [7 : 0] s_axis_tx_tkeep
@@ -284,42 +284,42 @@ axi_10g_ethernet_nonshared u_axi_10g_ethernet_1 (
 	.s_axis_tx_tready        (m_axis_rx1_tready),       // output wire s_axis_tx_tready
 	.s_axis_tx_tuser         (m_axis_rx1_tuser),        // input wire [0 : 0] s_axis_tx_tuser
 	.s_axis_tx_tvalid        (m_axis_rx1_tvalid),       // input wire s_axis_tx_tvalid
-	.s_axis_pause_tdata      (s_axis_pause_tdata),     // input wire [15 : 0] s_axis_pause_tdata
-	.s_axis_pause_tvalid     (s_axis_pause_tvalid),    // input wire s_axis_pause_tvalid
+	.s_axis_pause_tdata      (16'd0),     // input wire [15 : 0] s_axis_pause_tdata
+	.s_axis_pause_tvalid     (1'd0),    // input wire s_axis_pause_tvalid
 	.m_axis_rx_tdata         (s_axis_tx1_tdata),        // output wire [63 : 0] m_axis_rx_tdata
 	.m_axis_rx_tkeep         (s_axis_tx1_tkeep),        // output wire [7 : 0] m_axis_rx_tkeep
 	.m_axis_rx_tlast         (s_axis_tx1_tlast),        // output wire m_axis_rx_tlast
 	.m_axis_rx_tuser         (s_axis_tx1_tuser),        // output wire m_axis_rx_tuser
 	.m_axis_rx_tvalid        (s_axis_tx1_tvalid),       // output wire m_axis_rx_tvalid
-	.tx_statistics_valid     (tx_statistics_valid),    // output wire tx_statistics_valid
-	.tx_statistics_vector    (tx_statistics_vector),   // output wire [25 : 0] tx_statistics_vector
-	.rx_statistics_valid     (rx_statistics_valid),    // output wire rx_statistics_valid
-	.rx_statistics_vector    (rx_statistics_vector)    // output wire [29 : 0] rx_statistics_vector
+	.tx_statistics_valid     (),    // output wire tx_statistics_valid
+	.tx_statistics_vector    (),   // output wire [25 : 0] tx_statistics_vector
+	.rx_statistics_valid     (),    // output wire rx_statistics_valid
+	.rx_statistics_vector    ()    // output wire [29 : 0] rx_statistics_vector
 );
 
 /*
  * Loopback FIFO
  */
-//axis_data_fifo_1 u_axis_data_fifo (
-//  .s_axis_aresetn(!eth_rst),          // input wire s_axis_aresetn
-//  .s_axis_aclk(clk156),                // input wire s_axis_aclk
-//  .s_axis_tvalid(s_axis_tvalid),            // input wire s_axis_tvalid
-//  .s_axis_tready(),            // output wire s_axis_tready
-//  .s_axis_tdata(s_axis_tdata),              // input wire [63 : 0] s_axis_tdata
-//  .s_axis_tkeep(s_axis_tkeep),              // input wire [7 : 0] s_axis_tkeep
-//  .s_axis_tlast(s_axis_tlast),              // input wire s_axis_tlast
-//  .s_axis_tuser(1'b0),              // input wire [0 : 0] s_axis_tuser
-//  .m_axis_tvalid(m_axis_tvalid),            // output wire m_axis_tvalid
-//  .m_axis_tready(m_axis_tready),            // input wire m_axis_tready
-//  .m_axis_tdata(m_axis_tdata),              // output wire [63 : 0] m_axis_tdata
-//  .m_axis_tkeep(m_axis_tkeep),              // output wire [7 : 0] m_axis_tkeep
-//  .m_axis_tlast(m_axis_tlast),              // output wire m_axis_tlast
-//  .m_axis_tuser(m_axis_tuser),              // output wire [0 : 0] m_axis_tuser
-//  .axis_data_count(),        // output wire [31 : 0] axis_data_count
-//  .axis_wr_data_count(),  // output wire [31 : 0] axis_wr_data_count
-//  .axis_rd_data_count()  // output wire [31 : 0] axis_rd_data_count
-//);
-//
+axis_data_fifo_1 u_axis_data_fifo (
+  .s_axis_aresetn(!eth_rst),          // input wire s_axis_aresetn
+  .s_axis_aclk(clk156),                // input wire s_axis_aclk
+  .s_axis_tvalid(s_axis_tx1_tvalid),            // input wire s_axis_tvalid
+  .s_axis_tready(),            // output wire s_axis_tready
+  .s_axis_tdata(s_axis_tx1_tdata),              // input wire [63 : 0] s_axis_tdata
+  .s_axis_tkeep(s_axis_tx1_tkeep),              // input wire [7 : 0] s_axis_tkeep
+  .s_axis_tlast(s_axis_tx1_tlast),              // input wire s_axis_tlast
+  .s_axis_tuser(1'b0),              // input wire [0 : 0] s_axis_tuser
+  .m_axis_tvalid(m_axis_rx1_tvalid),            // output wire m_axis_tvalid
+  .m_axis_tready(m_axis_rx1_tready),            // input wire m_axis_tready
+  .m_axis_tdata(m_axis_rx1_tdata),              // output wire [63 : 0] m_axis_tdata
+  .m_axis_tkeep(m_axis_rx1_tkeep),              // output wire [7 : 0] m_axis_tkeep
+  .m_axis_tlast(m_axis_rx1_tlast),              // output wire m_axis_tlast
+  .m_axis_tuser(m_axis_rx1_tuser),              // output wire [0 : 0] m_axis_tuser
+  .axis_data_count(),        // output wire [31 : 0] axis_data_count
+  .axis_wr_data_count(),  // output wire [31 : 0] axis_wr_data_count
+  .axis_rd_data_count()  // output wire [31 : 0] axis_rd_data_count
+);
+
 /*
  * Loopback FIFO for packet mode
  */
